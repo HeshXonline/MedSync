30c249ecd6e61892bd1bb63a9a130457e00204e8
HeshanNethmina <heshannethmina169@gmail.com>
2025-08-17 21:25:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // Remove StrictMode temporarily while debugging to avoid double effects
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
